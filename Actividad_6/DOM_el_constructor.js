3d09af07d6ed46b6536469efb80f684d9417a21d
Carlos-V24 <vihcprepa6@comunidad.unam.mx>
2020-06-15 10:08:25 -0500

    Se añadio la actividad 6

---
+++ Actividad_6/DOM_el_constructor.js
@@ -0,0 +1,7 @@
+document.write("<div style='background: #c2c7cf; border: thick dashed #191920;''>");
+document.write("<h1><a href='#'>Soy el más grande...</a></h1>");
+document.write("<h2>Curso Web 2020</h2>");
+document.write("<h2><i><a href='#'>Mediano</a></i></h2>");
+document.write("<h4 style='color: red'>Jelou</h4>");
+document.write("<h3>Estoy medio chiquito :(</h3>");
+document.write("<div>");
